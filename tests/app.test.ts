fe6a95d7e42475684f224d37a1d7378d0ed5934f
Tina Oluwabukola <oluwabukolatina@gmail.com>
2020-07-21 23:17:34 +0100

    test passing

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,13 +1,14 @@
-import * as chai from 'chai';
+import chai from 'chai';
+import 'chai/register-should';
+import chaiHttp from 'chai-http';
 import app from '../src/app';
-import 'mocha';
-import chaiHttp = require('chai-http');
 
 chai.use(chaiHttp);
-const { expect } = chai;
 
-describe('Test', () => {
-  it('should return response on call of base url /', () => chai.request(app).get('/')
+describe('Hello API Request', () => {
+  it('should return response on call', () => chai
+    .request(app)
+    .get('/')
     .then((res) => {
       console.log(res.text);
       chai.expect(res.text).to.eql('Hello Node/Typescript starter!');
