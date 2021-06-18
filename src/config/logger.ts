759a4f3bbf08be76e8c87e1e3f2e4766a37d74e2
oluwabukola tina <oluwabukolatina@gmail.com>
2021-06-18 23:19:00 +0100

    Update logger.ts

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -56,6 +56,9 @@ const options: winston.LoggerOptions = {
       level: ENVIRONMENT === 'production' ? 'error' : 'debug',
       format,
     }),
+        // - Write all logs with level `error` and below to `error.log`
+    // - Write all logs with level `info` and below to `combined.log`
+    //
     new transports.File({ filename: 'logs/debug.log', level: 'error' }),
     new transports.File({ filename: 'logs/combined.log' }),
   ],
